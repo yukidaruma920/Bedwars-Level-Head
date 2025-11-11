826e089dfea21bbbfe077a7ea27db016ac893ec7
yukidaruma920 <yuki.920.daruma@gmail.com>
2025-11-11 14:08:48 +0900

    fix build.gradle

--- src/main/java/com/yuki920/levelhead/LevelHeadMod.java
+++ src/main/java/com/yuki920/levelhead/LevelHeadMod.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 public class LevelHeadMod
 {
     public static final String MODID = "levelhead";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.7.0";
     public static Logger logger;
     
     // RenderHandlerインスタンスを保持
